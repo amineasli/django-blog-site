f7127b5a5d9dd137582153947f2b13537c5fff26
Amine Asli <mhammed.amine.asli@gmail.com>
2021-04-21 17:34:51 +0000

    Only list posts with status = 'published'

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from .models import Post
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(status='published')
     context = {'posts': posts}
     return render(request, 'blog/index.html', context)
