bf8f17b078d8fdebd27669c63303e15dbb506dc4
Amine Asli <mhammed.amine.asli@gmail.com>
2021-04-21 15:49:29 +0000

    Create the index view and configure the URLConf for this view

--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,3 +1,9 @@
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def index(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blog/index.html', context)
