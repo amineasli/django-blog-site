899ec4c5e998a5c9d894d1f323319b626de5004c
Amine Asli <mhammed.amine.asli@gmail.com>
2021-04-22 00:47:33 +0000

    Create detail view and it's corresponding URLConf and template

--- blogsite/blog/urls.py
+++ blogsite/blog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('<int:year>/<int:month>/<int:day>/<slug:slug>/',
+        views.detail,
+        name='detail'),
     path('', views.index, name='index'),
 ]   
--- blogsite/blog/views.py
+++ blogsite/blog/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import ListView, DetailView
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 
@@ -7,3 +7,12 @@ def index(request):
     posts = Post.objects.filter(status='published')
     context = {'posts': posts}
     return render(request, 'blog/index.html', context)
+
+def detail(request, year, month, day, slug):
+    post = get_object_or_404(Post, slug=slug,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
