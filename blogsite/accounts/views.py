8d199b5e3ccfd001eab943067b6d5b2fe7443096
Amine Asli <mhammed.amine.asli@gmail.com>
2021-04-24 06:30:38 +0000

    Extend UserCreationForm by adding custom field 'email'

--- blogsite/accounts/views.py
+++ blogsite/accounts/views.py
@@ -1,12 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'registration/signup.html', {'form': form})
