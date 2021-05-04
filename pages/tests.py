4d00a01b34a607bb770298204b813f82e48301f7
Amine Asli <mhammed.amine.asli@gmail.com>
2021-05-04 00:38:18 +0000

    Add tests for pages app

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,9 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SimpleTests(SimpleTestCase):
+    databases = '__all__'
+
+    def test_about_page_status_code(self):
+        response = self.client.get(reverse('pages:about_page'))
+        self.assertEqual(response.status_code, 200)
